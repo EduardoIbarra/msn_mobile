d42d191d1b6b5c0b81d45e2b4b5588a3b84eb97e
Eduardo <ibredu@gmail.com>
2018-06-07 12:04:50 -0500

    Installed angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,20 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { UserService } from '../services/user.service';
+import { ConversationService } from '../services/conversation.service';
+import { RequestService } from '../services/request.service';
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB7l5cIu6b7OGahxBtZWhER3vqV6xU6-lA',
+  authDomain: 'msnp-fdd0e.firebaseapp.com',
+  databaseURL: 'https://msnp-fdd0e.firebaseio.com',
+  projectId: 'msnp-fdd0e',
+  storageBucket: 'msnp-fdd0e.appspot.com',
+  messagingSenderId: '368905821941'
+};
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +31,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,7 +44,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserService,
+    ConversationService,
+    RequestService
   ]
 })
 export class AppModule {}
---
+++ src/services/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable()
+export class UserService {
+
+  constructor(private afDb: AngularFireDatabase, private afAuth: AngularFireAuth) { }
+  registerWithEmailAndPassword (user) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+  }
+  signInWithEmailAndPassword (user) {
+    return this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
+  createUser(user) {
+    return this.afDb.object('users/' + user.uid).set(user);
+  }
+  getUsers() {
+    return this.afDb.list('users/');
+  }
+  getUser(uid) {
+    return this.afDb.object('users/' + uid);
+  }
+  setUserProperty(key, value, uid) {
+   return this.afDb.object('users/' + uid + '/' + key).set(value);
+  }
+  addFriend(uid, friendId) {
+    this.afDb.object('users/' + uid + '/friends/' + friendId).set(friendId);
+    return this.afDb.object('users/' + friendId + '/friends/' + uid).set(uid);
+  }
+}
